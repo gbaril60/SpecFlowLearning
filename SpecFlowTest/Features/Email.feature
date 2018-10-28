52ba4474953e1460c818f61a3abb6e61f9f900f6
Gary Baril <gdaril60@gmail.com>
2018-10-28 11:14:13 -0400

    Adding Projects to repository

---
+++ SpecFlowTest/Features/Email.feature
@@ -0,0 +1,18 @@
+﻿Feature: Send Email
+	As a user of the application
+	In order to communicate effectively
+	I want to be able to send an email from the application
+
+@Email
+Scenario Outline: Add a list of numbers
+	Given I have entered <number1> into the calculator
+	And I have also entered <number2> into the calculator
+	When I press add
+	Then the result should be <answer> on the screen
+
+	Examples: 
+	| number1 | number2 | answer |
+	| 1       | 2       | 3      |
+	| 2       | 2       | 4      |
+	| 4       | 5       | 9      |
+	| -1      | 8       | 7      |
---
+++ SpecFlowTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpecFlowTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpecFlowTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6743a0d9-9214-49e4-a4ec-b0196790d291")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
